690b953ae710296906130d2935118e69e017ee3c
Mohammed Faiz <77430169+mdfaiz027@users.noreply.github.com>
2022-10-14 11:58:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.practice;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+}
